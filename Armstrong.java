9080b9be8d00bc21c92074109f319899987a9f88
bommisettiasha-create <bommisettiasha@gmail.com>
2026-07-09 22:03:59 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,19 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 153;
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum = sum + (digit * digit * digit);
+            num = num / 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+    }
+}
