2f8ae2cfcd9df84afb9d4e12949b5fc700a361f5
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-02 18:17:15 +0530

    Fix variable declaration for palindrome check

--- palindrome.java
+++ palindrome.java
@@ -2,7 +2,8 @@ import java.util.*;
  class Main{ public static void main(String[] args){ 
 Scanner sc = new Scanner(System.in);
  int n = sc.nextInt();
- int temp = n, rev = 0; 
+ int temp = n;
+  rev = 0; 
 while(n != 0){ 
 rev = rev * 10 + n % 10;
  n /= 10; 
