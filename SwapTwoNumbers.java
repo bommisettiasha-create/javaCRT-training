9f7e05a2351131c382b26afe8389c70ddd26e6bf
bommisettiasha-create <bommisettiasha@gmail.com>
2026-08-02 18:18:30 +0530

    Remove unnecessary line breaks in SwapTwoNumbers

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -1,13 +1,10 @@
 public class SwapTwoNumbers{
-    public static void main(String[] args) {
-        int a = 10, b = 20;
-
-        System.out.println("Before Swap: a = " + a + ", b = " + b);
-
-        int temp = a;
-        a = b;
-        b = temp;
-
-        System.out.println("After Swap: a = " + a + ", b = " + b);
+ public static void main(String[] args) {
+ int a = 10, b = 20;
+System.out.println("Before Swap: a = " + a + ", b = " + b);
+ int temp = a;
+ a = b;
+b = temp;
+ System.out.println("After Swap: a = " + a + ", b = " + b);
     }
 }
