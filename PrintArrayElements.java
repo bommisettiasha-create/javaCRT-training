ab84d0c25edc367000f21d0935c5f4582697019d
bommisettiasha-create <bommisettiasha@gmail.com>
2026-09-15 19:36:35 +0530

    Fix formatting issues in PrintArrayElements.java

--- PrintArrayElements.java
+++ PrintArrayElements.java
@@ -1,8 +1,7 @@
 public class PrintArray {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50};
-
-        for (int i = 0; i < arr.length; i++) {
+for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
