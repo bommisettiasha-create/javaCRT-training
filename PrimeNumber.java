b06d2c54cfb099ea1121507cc2a3dd1b79c833ce
bommisettiasha-create <bommisettiasha@gmail.com>
2026-09-24 22:12:03 +0530

    Refactor PrimeNumber.java for consistency
    Removed unnecessary whitespace and formatting inconsistencies.

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,20 +2,18 @@ public class PrimeNumber {
     public static void main(String[] args) {
         int n = 17;
         boolean prime = true;
-
-        if (n <= 1)
-            prime = false;
+ if (n <= 1)
+ prime = false;
         else {
             for (int i = 2; i <= n / 2; i++) {
                 if (n % i == 0) {
-                    prime = false;
-                    break;
+     prime = false;
+      break;
                 }
             }
         }
-
-        if (prime)
-            System.out.println("Prime");
+ if (prime)
+     System.out.println("Prime");
         else
             System.out.println("Not Prime");
     }
