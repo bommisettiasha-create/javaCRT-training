acc8a8eac0727e90aee8ca2a3fe88a533f474c9b
bommisettiasha-create <bommisettiasha@gmail.com>
2026-09-24 22:09:31 +0530

    Refactor Fibonacci class formatting

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,10 +2,8 @@ public class Fibonacci {
     public static void main(String[] args) {
         int n = 10;
         int a = 0, b = 1;
-
-        System.out.print(a + " " + b + " ");
-
-        for (int i = 3; i <= n; i++) {
+System.out.print(a + " " + b + " ");
+for (int i = 3; i <= n; i++) {
             int c = a + b;
             System.out.print(c + " ");
             a = b;
